28858d49a0f8eccbecfe32f66b2f1d8850fb0abe
ZWX544198 <hengwujun128@gmail.com>
2018-09-28 11:40:47 +0800

    add tree menu

---
+++ src/views/myDirective/navigation/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="navigation-container">
+    <tree-menu :label="treeData.label" :nodes="treeData.nodes" :depth="0"></tree-menu>
+    <!--  -->
+    <div></div>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+
+  </div>
+</template>
+<script>
+/**
+ * 
+ * 1.If you’re using a component recursively you must either register it globally with Vue.component, 
+ * or, give it a name property. Otherwise, any children of the component will not be able to resolve further invocations
+ *  and you'll get an undefined component error.
+ * 
+ */
+// 2.Remember that our data structure has a single root node. To begin the recursion we invoke the TreeMenu component in our main template, using the root nodesproperties for the props:
+
+import TreeMenu from './tree-menu.vue'
+export default {
+  components: { TreeMenu },
+  data () {
+    return {
+      // it must have a single root node but can be infinitely deep.
+      treeData: {
+        label: 'root',
+        nodes: [
+          {
+            label: 'item1',
+            nodes: [
+              {
+                label: 'item1.1'
+              },
+              {
+                label: 'item1.2',
+                nodes: [
+                  {
+                    label: 'item1.2.1'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            label: 'item2'
+          }
+        ]
+      },
+      data: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
+          }]
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
+          }]
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.navigation-container {
+  width: 100%;
+  height: 100vh;
+  padding: 10px;
+  text-align: left;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/myDirective/navigation/tree-menu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tree-menu">
+    <div :style="indent" @click="toogleChildren">{{label}}</div>
+    <!--If `nodes` is undefined this will not render-->
+    <tree-menu v-if="showChildren" v-for="node in nodes" :key="node.label" :label="node.label" :nodes="node.nodes" :depth="depth+1"></tree-menu>
+  </div>
+</template>
+<script>
+/**
+ * 
+ * If you’re using a component recursively you must either register it globally with Vue.component, 
+ * or, give it a name property. Otherwise, any children of the component will not be able to resolve further invocations
+ *  and you'll get an undefined component error.
+ * 
+ */
+export default {
+  name: 'tree-menu',
+  // recursive component
+  props: ['label', 'nodes', 'depth'],
+  data () {
+    return {
+      showChildren: false
+    }
+  },
+  computed: {
+    // style can be use computed property return an object
+    indent () {
+      return { transform: `translate(${this.depth * 20}px)` }
+    }
+  },
+  methods: {
+    toogleChildren () {
+      this.showChildren = !this.showChildren
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.tree-menu {
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
