4a4c918157bc65b18ad736b9b09e2e8971fcb39f
ZWX544198 <hengwujun128@gmail.com>
2018-10-22 14:46:22 +0800

    modify sth

--- src/router/image.js
+++ src/router/image.js
@@ -1,14 +1,21 @@
 import images from "@/views/images/index.vue"
 import slideShow from "@/views/images/slideShow.vue"
+import showPdf from "@/views/images/showPdf.vue"
 const imageRouter = [{
   path: '/images',
   name: 'images',
   component: images,
   redirect: 'slideShow',
   children: [{
-    path: 'slideShow',
-    name: 'slideShow',
-    component: slideShow
-  }]
+      path: 'slideShow',
+      name: 'slideShow',
+      component: slideShow
+    },
+    {
+      path: 'showPdf',
+      name: 'showPdf',
+      component: showPdf
+    }
+  ]
 }]
 export default imageRouter
---
+++ src/views/images/showPdf.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pdf">
+    <h2>hello,how to make vue to show pdf</h2>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'hello,this is title'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
