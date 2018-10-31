0f41ae201e29dd37f708918fe3308b519a44fb9f
zWX544198 <hengwujun128@gmail.com>
2018-10-31 11:03:40 +0800

    add sth

--- src/views/canban/index.old.js
+++ src/views/canban/index.old.js
@@ -4,11 +4,7 @@ const indexMixin = {
       groupId: 0,
       groupName: 'add group',
       dialogVisible: false,
-      operationTag: 'add',
-      addGroupTag: 1,
-      groupIndex: '',
       groupForm: {
-        title: '新增分组',
         name: ''
       },
       list: [{
@@ -113,66 +109,133 @@ const indexMixin = {
           ]
         }
       ],
-      list2: [],
+      list2: [{
+          groupId: '1',
+          groupName: 'first group',
+          title: 'first group',
+          isCollapse: true,
+          description: 'hello first',
+          parameterList: [{
+              parameterId: 'parameterId1',
+              parameterName: 'parameterName1'
+            },
+            {
+              parameterId: 'parameterId2',
+              parameterName: 'parameterName2'
+            },
+            {
+              parameterId: 'parameterId3',
+              parameterName: 'parameterName3'
+            },
+            {
+              parameterId: 'parameterId4',
+              parameterName: 'parameterName4'
+            }
+          ]
+        },
+        {
+          groupId: '2',
+          groupName: 'second group',
+          title: 'second group',
+          isCollapse: false,
+          description: 'hello second',
+          parameterList: [{
+              parameterId: 'parameterId12',
+              parameterName: 'parameterName12'
+            },
+            {
+              parameterId: 'parameterId22',
+              parameterName: 'parameterName22'
+            },
+            {
+              parameterId: 'parameterId32',
+              parameterName: 'parameterName32'
+            },
+            {
+              parameterId: 'parameterId42',
+              parameterName: 'parameterName42'
+            }
+          ]
+        },
+        {
+          groupId: '3',
+          groupName: 'third group',
+          title: 'third group',
+          isCollapse: false,
+          description: 'hello third',
+          parameterList: [{
+              parameterId: 'parameterId13',
+              parameterName: 'parameterName13'
+            },
+            {
+              parameterId: 'parameterId23',
+              parameterName: 'parameterName23'
+            },
+            {
+              parameterId: 'parameterId33',
+              parameterName: 'parameterName33'
+            },
+            {
+              parameterId: 'parameterId43',
+              parameterName: 'parameterName43'
+            }
+          ]
+        },
+        {
+          groupId: '4',
+          groupName: 'fourth group',
+          title: 'fourth group',
+          name: 'fourth',
+          description: 'hello fourth',
+          parameterList: [{
+              parameterId: 'parameterId14',
+              parameterName: 'parameterName14'
+            },
+            {
+              parameterId: 'parameterId24',
+              parameterName: 'parameterName24'
+            },
+            {
+              parameterId: 'parameterId34',
+              parameterName: 'parameterName34'
+            },
+            {
+              parameterId: 'parameterId44',
+              parameterName: 'parameterName44'
+            },
+            {
+              parameterId: 'parameterId54',
+              parameterName: 'parameterName54'
+            }
+
+          ]
+        }
+      ],
       list3: []
     }
   },
   methods: {
-    addGroup(tag) {
-      this.operationTag = 'add',
-        this.groupForm.title = '新增分组'
-      this.groupForm.name = ''
-      this.addGroupTag = tag;
+    addGroup() {
       this.dialogVisible = true
     },
-    editGroup(tag, index) {
-      this.operationTag = 'edit';
-      this.groupForm.title = '编辑分组'
-      // 
-      this.groupIndex = index
-      this.addGroupTag = tag;
-      if (tag == 1) {
-        this.groupForm.name = this.list2[index].groupName
-      } else {
-        this.groupForm.name = this.list3[index].groupName
-      }
+    editGroup() {
       this.dialogVisible = true
     },
-    deleteGroup(tag, index) {
-      if (tag === 1) {
-        this.list2.splice(index, 1)
-      } else {
-        this.list3.splice(index, 1)
-      }
+    deleteGroup(index) {
+      alert(123)
+      this.list2.splice(index, 1)
     },
     handleClose() {
       this.dialogVisible = false
     },
-    //
     saveGroup() {
-      // debugger
-      // 新增分组
-      if (this.operationTag === 'add') {
-        let group = {
-          groupId: -1,
-          groupName: this.groupForm.name,
-          parameterList: []
-        }
-        if (this.addGroupTag == 1) {
-          this.list2.push(group)
-        } else {
-          this.list3.push(group)
-        }
-      } else {
-        // 编辑分组
-        // 获取修改后的分组名称
-        if (this.addGroupTag == 1) {
-          this.list2[this.groupIndex]['groupName'] = this.groupForm.name
-        } else {
-          this.list3[this.groupIndex]['groupName'] = this.groupForm.name
-        }
+      this.groupId += 1;
+      let group = {
+        groupId: this.groupId,
+        groupName: this.groupForm.name,
+        parameterList: []
       }
-
+      this.list2.push(group)
       this.dialogVisible = false
     }
   }
